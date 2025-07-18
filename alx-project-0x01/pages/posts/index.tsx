6fdc4794846b4ba5bdfebd87c9b33ee2c1eae573
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-18 11:13:12 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-semibold">Posts Page</h1>
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-semibold">Posts Page</h1>
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-semibold">Users Page</h1>
+    <div>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-semibold">Users Page</h1>
+      </main>
     </div>
   );
 };
 
 export default Users;
-
